e7069bd4faba5acf7ad06e90eb3399635e8909f5
john omedo <johnomedo59@gmail.com>
2025-06-04 23:59:43 +0300

    payment.html

---
+++ jowins.js
@@ -0,0 +1,14 @@
+
+/*for payment.html*/
+function consult(){
+    let post = document.getElementById("pay1");
+    post = alert("Please pay 2000kshs through the paybill indicated;Then call/mail using the links below");
+  }
+  function image1(){
+    let post = document.getElementById("pay2");
+    post = alert("obstetric ultrasound imaging- 2500kshs /General pelvic ultrasound- 2000kshs");
+  }
+  function educate(){
+    let post = document.getElementById("pay3");
+    post = alert("Clinical tutorship/Theoretical class sessions will be charged as per starndard market hourly rates");
+  }
